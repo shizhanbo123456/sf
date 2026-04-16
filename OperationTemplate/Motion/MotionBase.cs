27d51fc2668b729b59db42474008d35485117de4
Zhanbo Shi <shizhanbo123456@163.com>
2026-04-16 15:33:54 +0800

    fix: target facing applied in motion

--- OperationTemplate/Motion/MotionBase.cs
+++ OperationTemplate/Motion/MotionBase.cs
@@ -17,6 +17,14 @@ namespace Variety.Base
             {6,12 },
             {7,18 },
             {8,24 },
+        }; 
+        private static readonly Dictionary<int, float> ShortSpeedMap = new()
+        {
+            {0,-12 },
+            {1,-6 },
+            {2,0 },
+            {3,6 },
+            {4,12 },
         };
 
         public bool IsNull => Time.time > workTime + startTime;
@@ -37,7 +45,7 @@ namespace Variety.Base
         public MotionBase(ushort id)
         {
             startTime = Time.time;
-            MotionIdCalculater.mod.Decode(id, out var timeFactor, out var stoicLevel, out int code);
+            MotionIdCalculator.mod.Decode(id, out var timeFactor, out var stoicLevel, out int code);
             workTime = timeFactor * 0.2f + 0.2f;
             this.stoicLevel = stoicLevel;
             if (code == 0)
@@ -52,7 +60,7 @@ namespace Variety.Base
             {
                 type = 1;
                 code -= 1;
-                MotionIdCalculater.speedV2Mod.Decode(code, out lx, out ly);
+                MotionIdCalculator.speedV2Mod.Decode(code, out lx, out ly);
                 hx = 0;
                 hy = 0;
             }
@@ -60,7 +68,7 @@ namespace Variety.Base
             {
                 type = 2;
                 code -= 82;
-                MotionIdCalculater.speedV2Mod.Decode(code, out lx, out ly);
+                MotionIdCalculator.speedV2Mod.Decode(code, out lx, out ly);
                 hx = 0;
                 hy = 0;
             }
@@ -68,13 +76,13 @@ namespace Variety.Base
             {
                 type = 3;
                 code -= 163;
-                MotionIdCalculater.speedV4Mod.Decode(code, out lx, out ly,out hx,out hy);
+                MotionIdCalculator.speedV4Mod.Decode(code, out lx, out ly,out hx,out hy);
             }
             else if (code <= 868)
             {
                 type = 4;
                 code -= 788;
-                MotionIdCalculater.speedV2Mod.Decode(code, out lx, out ly);
+                MotionIdCalculator.speedV2Mod.Decode(code, out lx, out ly);
                 hx = 0;
                 hy = 0;
             }
@@ -85,41 +93,41 @@ namespace Variety.Base
         }
 
         //传入当前速度，传出生效速度
-        public Vector2 GetVelocity(Vector2 v)
+        public Vector2 GetVelocity(Vector2 v, bool faceRight)
         {
             switch (type)
             {
                 case 1:return Vector2.zero;
-                case 2:return new Vector2(SpeedMap[lx],SpeedMap[ly]);
-                case 3:return v + new Vector2(SpeedMap[lx], SpeedMap[ly]) * Time.deltaTime;
-                case 4:return Vector2.Lerp(new Vector2(SpeedMap[lx], SpeedMap[ly]), new Vector2(SpeedMap[hx], SpeedMap[hy]), SpawnTime / workTime);
+                case 2:return new Vector2(faceRight ? SpeedMap[lx] : -SpeedMap[lx], SpeedMap[ly]);
+                case 3:return v + new Vector2(faceRight ? SpeedMap[lx] : -SpeedMap[lx], SpeedMap[ly]) * Time.deltaTime;
+                case 4:return Vector2.Lerp(new Vector2(faceRight ? ShortSpeedMap[lx] : -ShortSpeedMap[lx], ShortSpeedMap[ly]), new Vector2(faceRight ? ShortSpeedMap[hx] : -ShortSpeedMap[hx], ShortSpeedMap[hy]), SpawnTime / workTime);
                 case 5:return v;
             }
             Debug.LogError("错误的type");
             return v;
         }
 
-        public Vector2 Entry(Vector2 v)
+        public Vector2 Entry(Vector2 v, bool faceRight)
         {
             switch (type)
             {
                 case 1: return Vector2.zero;
-                case 2: return new Vector2(SpeedMap[lx], SpeedMap[ly]);
+                case 2: return new Vector2(faceRight ? SpeedMap[lx] : -SpeedMap[lx], SpeedMap[ly]);
                 case 3: return v;
-                case 4: return new Vector2(SpeedMap[lx], SpeedMap[ly]);
+                case 4: return new Vector2(faceRight ? ShortSpeedMap[lx] : -ShortSpeedMap[lx], ShortSpeedMap[ly]);
                 case 5: return new Vector2(SpeedMap[lx], SpeedMap[ly]);
             }
             Debug.LogError("错误的type");
             return v;
         }
-        public Vector2 Exit(Vector2 v)
+        public Vector2 Exit(Vector2 v, bool faceRight)
         {
             switch (type)
             {
                 case 1: return Vector2.zero;
-                case 2: return new Vector2(SpeedMap[lx], SpeedMap[ly]);
+                case 2: return new Vector2(faceRight?SpeedMap[lx]:-SpeedMap[lx], SpeedMap[ly]);
                 case 3: return v;
-                case 4: return new Vector2(SpeedMap[hx], SpeedMap[hy]);
+                case 4: return new Vector2(faceRight?ShortSpeedMap[hx]:-ShortSpeedMap[hx], ShortSpeedMap[hy]);
                 case 5: return v;
             }
             Debug.LogError("错误的type");
