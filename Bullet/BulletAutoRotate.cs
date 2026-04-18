4e750b143669ba10be2e45893c9c8f8c37f883bb
Zhanbo Shi <shizhanbo123456@163.com>
2026-04-18 12:15:07 +0800

    fix: bullet rotate error

--- Bullet/BulletAutoRotate.cs
+++ Bullet/BulletAutoRotate.cs
@@ -5,15 +5,6 @@ using UnityEngine;
 public class BulletAutoRotate : MonoBehaviour
 {
     private Vector3 lastPos;
-    private Vector3 scale;
-    private Vector3 iscale;
-    private void Awake()
-    {
-        lastPos=transform.position;
-        scale = transform.localScale;
-        iscale=transform.localScale;
-        iscale.x *= -1;
-    }
     private void Update()
     {
         var p=transform.position-lastPos;
@@ -23,13 +14,17 @@ public class BulletAutoRotate : MonoBehaviour
             float angle = Mathf.Atan(p.y / p.x)*Mathf.Rad2Deg;
             if (p.x > 0)
             {
-                transform.localScale = scale;
+                var s = transform.localScale;
+                s.x = Mathf.Abs(s.x);
+                transform.localScale = s;
                 transform.rotation = Quaternion.Euler(0, 0, angle);
             }
             else
             {
-                transform.localScale = iscale;
-                transform.rotation = Quaternion.Euler(0, 0, -angle);
+                var s = transform.localScale;
+                s.x = -Mathf.Abs(s.x);
+                transform.localScale = s;
+                transform.rotation = Quaternion.Euler(0, 0, angle);
             }
         }
     }
